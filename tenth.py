e6fc062bb46d4f966582b3d1dffa8129a37b6f23
elormd <dziks20052@gmail.com>
2020-10-16 13:38:42 +0000

    this is exercise 20

---
+++ tenth.py
@@ -0,0 +1,12 @@
+for i in 'portland':
+    print(i)
+for i in range(1,10):
+    print('i')
+for i in range(10):
+    print(i)
+for i in range(3 , 0, -1):
+    print(i)
+name = 'elorm'
+for i in range(3):
+    for i in name:
+        print (i)
